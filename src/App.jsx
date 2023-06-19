353dcec4bd4524b594bd8230e8394f81de999f77
Leah Cardoz <leahcardoz@gmail.com>
2023-06-19 17:44:43 -0400

    practicing flexbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Pic from '../public/flexboxvsgrid.png';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div>
+      <div id="app">
+        <h1>Flexbox vs. Grid</h1>
+        <img id="pic" src={Pic} alt="flexbox vs grid"/>
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Flexbox</h2>
+          <div className="flexbox-container">
+            <div className="flexbox-item flexbox-item-1"></div>
+            <div className="flexbox-item flexbox-item-2"></div>
+            <div className="flexbox-item flexbox-item-3"></div>
+          </div>
+        <h2>Grid</h2>
+          <div className="grid-container">
+          
+          </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
